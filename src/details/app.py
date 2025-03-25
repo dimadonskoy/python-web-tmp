7f45b306b30210455eba06fc72280247b96c404c
Dmitri Donskoy <crooper22@gmail.com>
2025-03-25 18:12:24 +0200

    sdsd

--- src/details/app.py
+++ src/details/app.py
@@ -4,22 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
-def index():
-    return 'Hello world'
-
-@app.route('/<name>')
-def hello(name):
-    return f'Hello {name}'
-
-@app.route('/check_file/<file_name>')
-def check_file(file_name):
-    try:
-        with open(file_name) as file:
-            data = file.readlines()
-        return data
-    except Exception as e:
-        return f'[!] Error: {e}'
+def hello():
+    return f'<H1>Hello world</H1>'
 
 
-app.run(host='0.0.0.0' , port=8000 , debug=True)
+app.run(host='0.0.0.0', port=8000, debug=True)
